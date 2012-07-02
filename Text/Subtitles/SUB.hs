53157a29ba3d415b0bdf4ac419055e3cb0357e03
ruben.astud <ruben.astud@gmail.com>
2012-07-02 07:24:16 +0000

    SUBdoc: generated haddock documentation for SUB modules.

--- Text/Subtitles/SUB.hs
+++ Text/Subtitles/SUB.hs
@@ -10,6 +10,13 @@
 
 module Text.Subtitles.SUB 
   (
+  -- * Terminology of the module
+  -- $example 
+
+  -- *Re-exported Datatypes
+  module Text.Subtitles.SUB.Datatypes,
+  
+  -- * Main Parsers
   parseSingleLine,
   parseSUB 
   ) where
@@ -19,11 +26,24 @@ import Data.Attoparsec.Text
 
 import Text.Subtitles.SUB.Datatypes
 
+-- $example
+--
+-- Refering to the parts of a single line of a subtitle file.
+--
+-- >{50}{100}Drama here!
+--
+-- the first to numbers correspond to the frame in which the text Drama here!
+-- is displayed. 
+
 frame :: Parser Frame
 frame = char '{' *> decimal <* char '}'
 
+-- | Given the example return the corresponding Line representation. At the
+-- moment this not handles modifiers as underlines or bold text
 parseSingleLine :: Parser Line 
 parseSingleLine = Line <$> frame <*> frame <*> takeTill isEndOfLine
 
+-- | Main parser of .sub files, given a .sub file it return a list of the dialog
+-- lines
 parseSUB :: Parser Subtitles
 parseSUB = sepBy1 parseSingleLine endOfLine
--- Text/Subtitles/SUB/Datatypes.hs
+++ Text/Subtitles/SUB/Datatypes.hs
@@ -6,14 +6,15 @@
 -- Maintainer  : ruben.astud@gmail.com
 -- Portability : unknown
 --
--- ADT for .sub files. Also serves as a place to  provide instance
+-- ADT for .sub files. Also serves as a place to  provide instance 
 -- declarations for the ADTs.
 
 module Text.Subtitles.SUB.Datatypes 
   (
-  -- * Re-exported datatypes.
+  -- * Re-exported datatypes
+  module Data.Attoparsec.Text,
   module Data.Text,
-
+  
   -- * Datatypes
   Frame,
   Subtitles,
@@ -21,9 +22,11 @@ module Text.Subtitles.SUB.Datatypes
   ) where
 
 import Data.Text (Text)
+import Data.Attoparsec.Text (Parser)
 
 type Subtitles = [Line]
 
+{- Should this be a newtype? or just a type? -}
 type Frame = Int
 
 data Line = Line { startFrame :: Frame,
