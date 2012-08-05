c89e6ba35d72292b52b5c8f4a3227f0fcdd6a1fe
ruben.astud <ruben.astud@gmail.com>
2012-08-05 03:14:35 +0000

    plain show instances SRT
    the purpose of this package is to PARSE not to prettyprint nor output writeable
    SRT code. so the show instance should be closer to what the programmer will be
    dealing with.

--- Text/Subtitles/SRT/Datatypes.hs
+++ Text/Subtitles/SRT/Datatypes.hs
@@ -31,12 +31,12 @@ data Time = Time
   , minutes :: Int
   , seconds :: Int
   , frame   :: Int
-  } deriving (Eq, Ord)
+  } deriving (Eq, Ord, Show)
 
 data Range = Range 
   { from :: Time
   , to   :: Time
-  } deriving (Eq, Ord)
+  } deriving (Eq, Ord, Show)
 
 -- | The core of the parser. each one of the constructor representing one part
 -- of the Line
@@ -44,27 +44,7 @@ data Line = Line
   { index  :: Int
   , range  :: Range
   , dialog   :: Text
-  } deriving (Eq, Ord)
+  } deriving (Eq, Ord, Show)
 
 type Subtitles = [Line]
 
-instance Show Time where
-  show (Time h m s f) = concat [showT h, ":", showT m, ":", showT s, ",", showF f]
-
-instance Show Range where
-  show (Range f t) = concat [show f, " --> ", show t]
-
-instance Show Line where
-  show (Line i t s) = intercalate "\n" (show i : show t : [unpack s]) ++ "\n"
-
-{- showT stands for showTime. In the parsing process "00" is read as "0", and
- - that tailing 0 is lost unless we add it manually when showing -}
-showT :: Int -> String
-showT a | a < 10    = '0' : show a
-        | otherwise = show a
-
-{- showF stands for showFrame -}
-showF :: Int -> String
-showF a | a < 10    = '0':'0': show a
-        | a < 100   = '0': show a
-        | otherwise = show a
